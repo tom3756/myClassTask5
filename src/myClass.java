7b1c8bf3b500721350707f39f28d572a8ceead69
tom3756 <tlees43@gmail.com>
2023-04-18 20:57:22 +0100

    Initial commit

---
+++ src/myClass.java
@@ -0,0 +1,10 @@
+public class myClass {
+    int x;
+    public myClass () {
+        x = 5;
+    }
+    public static void main(String[] args) {
+        myClass myObj = new myClass();
+        System.out.println(myObj.x);
+    }
+}
